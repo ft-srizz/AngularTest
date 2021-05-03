c692a61914d5ebe09cd8018e3af0cbf7a7c223b3
Srijan Tripathi <srijantripathi09@gmail.com>
2021-05-03 23:40:02 +0530

    Before using services

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,3 +1,5 @@
+import { Comment } from './comment';
+
 export class Dish {
     id: string | undefined;
     name: string | undefined;
@@ -7,4 +9,5 @@ export class Dish {
     label: string | undefined;
     price: string | undefined;
     description: string | undefined;
+    comments: Comment[] | undefined;
 }
