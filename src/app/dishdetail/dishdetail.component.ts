3bc9dda0eca133df83d2f9a1d2e3029e895666b2
Srijan Tripathi <srijantripathi09@gmail.com>
2021-05-10 09:18:09 +0530

    Router Parameter Dividing Menu And DishDetail

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { DishService } from '../services/dish.service';
 import { Dish } from '../shared/dish';
+
 
 
 
@@ -9,13 +13,23 @@ import { Dish } from '../shared/dish';
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
-  
-    @Input()
-    dish: Dish;
 
-  constructor() { }
+    dish: Dish;
+    dishIds: string[];
+    prev: string;
+    next: string;
+
+  constructor(private dishServie: DishService,
+    private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.params['id'];
+    this.dishServie.getDish(id)
+      .subscribe(dish => this.dish = dish);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
   
