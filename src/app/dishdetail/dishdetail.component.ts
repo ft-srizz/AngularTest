8b016f9191cf5752a6488066ad91e2d2c2c30d2f
Srijan Tripathi <srijantripathi09@gmail.com>
2021-05-10 09:40:12 +0530

    RXJS P2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,6 +3,7 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { DishService } from '../services/dish.service';
 import { Dish } from '../shared/dish';
+import { switchMap } from 'rxjs/operators';
 
 
 
@@ -19,14 +20,20 @@ export class DishdetailComponent implements OnInit {
     prev: string;
     next: string;
 
-  constructor(private dishServie: DishService,
+  constructor(private dishService: DishService,
     private route: ActivatedRoute, private location: Location) { }
 
-  ngOnInit(): void {
-    const id = this.route.snapshot.params['id'];
-    this.dishServie.getDish(id)
-      .subscribe(dish => this.dish = dish);
-  }
+    ngOnInit() {
+      this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+      this.route.params.pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
+      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+    }
+
+    setPrevNext(dishId: string) {
+      const index = this.dishIds.indexOf(dishId);
+      this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+      this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
+    }
 
   goBack(): void {
     this.location.back();
--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,13 +1,13 @@
 import { Comment } from './comment';
 
 export class Dish {
-    id: string | undefined;
-    name: string | undefined;
-    image: string | undefined;
-    category: string | undefined;
-    featured: boolean | undefined;
-    label: string | undefined;
-    price: string | undefined;
-    description: string | undefined;
-    comments: Comment[] | undefined;
+    id: string;
+    name: string;
+    image: string;
+    category: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    description: string;
+    comments: Comment[];
 }
